1343e311c0912acbb450e24694ba4a1746e75328
Myint Myat Thurein <myintmyatthurein@ad.gicjp.com>
2021-12-02 14:17:14 +0900

    Revert "changes for v2"
    This reverts commit 8834cf2e72232d45de49ce5216e607ce431e0027.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
@@ -8,11 +7,29 @@ function App() {
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
+<<<<<<< HEAD
         <h1>We now have Auth!</h1>
+=======
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+>>>>>>> parent of 8834cf2 (changes for v2)
       </header>
       <AmplifySignOut />
     </div>
   );
 }
 
+<<<<<<< HEAD
 export default withAuthenticator(App);
+=======
+export default App;
+>>>>>>> parent of 8834cf2 (changes for v2)
